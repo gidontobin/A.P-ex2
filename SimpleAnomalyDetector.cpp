95ed7de99439d5b43d6802cbd87df524d7784faa
Jucha <yohayartzi@gmail.com>
2021-11-15 10:46:16 +0200

    I'm here comment

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -10,7 +10,7 @@ SimpleAnomalyDetector::~SimpleAnomalyDetector() {
 	// TODO Auto-generated destructor stub
 }
 
-// I'm here 1 2
+// I'm here 1 2 3
 void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
 	// TODO Auto-generated destructor stub
 
