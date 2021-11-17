aa6f8f26ba2d19fa7f352f66a2118c9405f6249e
Gidon Tobin <gidontobin@gmail.com>
2021-11-17 11:46:59 +0200

    updated learnNormal function, and did detect function

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -1,6 +1,8 @@
 
 #include "SimpleAnomalyDetector.h"
 #include "anomaly_detection_util.h"
+#include <map>
+#include <vector>
 
 SimpleAnomalyDetector::SimpleAnomalyDetector() {
 	// TODO Auto-generated constructor stub
@@ -10,35 +12,33 @@ SimpleAnomalyDetector::SimpleAnomalyDetector() {
 SimpleAnomalyDetector::~SimpleAnomalyDetector() {
 	// TODO Auto-generated destructor stub
 }
-
-// Yohay The Fox
 void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
-	// TODO Auto-generated destructor stub
-
-	float* matrix = ts.getMat();
-	int num_of_colums = ts.size();
+	map<string,vector<float>> map_elements = ts.getMap();
+	int num_of_keys = ts.size();
 	int num_of_rows = ts.length();
 	float threshold = 0.9;
-	for(int i = 0; i<num_of_colums; i++) {
-        float maxCor = 0;
-        int corTo = -1;
 
-        for (int j = i + 1; j < num_of_colums; j++) {
-            float cor = pearson(matrix[1][i], matrix[1][j], num_of_rows);
+	for(map<string,vector<float>>::iterator header : map_elements) {
+        float maxCor = 0;
+        string corTo = "";
+
+        for (map<string,vector<float>>::iterator next = header+1;next!=map_elements.end();next++) {
+            float cor = pearson(header->second,next->second, num_of_rows);
             if(cor<0)
                 cor = -cor;
 
             if (cor > maxCor && cor>=threshold) {
                 maxCor = cor;
-                corTo = j;
+                corTo = next->first;
             }
         }
 
-        if (corTo==-1)
+        if (corTo=="")
             continue;
         Point points[num_of_rows];
-        for(int x = 1; x<num_of_rows; x++) {
-            points[x] = new Point(matrix[x][i],matrix[x][corTo]);
+
+        for(int i=0;i<num_of_rows;i++) {
+            points[i] = new Point(header->second[i],map_elements[corTo][i]);
         }
 
         float maxDev = 0;
@@ -51,8 +51,9 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
 
         maxDev=maxDev*1.1;
         correlatedFeatures corOfI;
-        corOfI.feature1 = matrix[0][i];
-        corOfI.feature2 = matrix[0][corTo];
+
+        corOfI.feature1 = header->first;
+        corOfI.feature2 = corTo;
         corOfI.corrlation = maxDev;
         corOfI.lin_reg = linear_reg;
         corOfI.threshold = threshold;
@@ -62,9 +63,37 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
 }
 
 vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts){
-	// TODO Auto-generated destructor stub
+    vector<AnomalyReport> report;
+	map<string,vector<float>> map_elements = ts.getMap();
+
+	int num_of_keys = ts.size();
+    int num_of_rows = ts.length();
+
+    //check every pair that was found as cor in learnNormal function
     for(correlatedFeatures corOfI : this->cf){
 
+        //make array of points
+        Point points[num_of_rows];
+        for(int i=0;i<num_of_rows;i++) {
+            points[i] = new Point(map_elements[corOfI.feature1][i],map_elements[corOfI.feature2][i]);
+        }
+
+        //get worst point
+        float maxDev = 0;
+        int badIndex = 0;
+        for(int i=0;i<num_of_rows;i++) {
+            float getDev = dev(points[i],corOfI.lin_reg);
+            if(getDev>maxDev){
+                maxDev=getDev;
+                badIndex = i;
+            }
+        }
+
+        //check if point is really bad, and if so, add a report to the vector of reports
+        if (maxDev>corOfI.corrlation){
+            report.push_back(new AnomalyReport(corOfI.feature1+"-"+corOfI.feature2,badIndex));
+        }
     }
+    return report;
 }
 
